a16bb1bd819e831ddc0bff0d6cbcd9702f16f9e3
Benoît Béranger <benoit.beranger@icm-institute.org>
2022-09-07 14:49:40 +0200

    plot is  very rudimental, but it's working

--- +mpd/@diagram/diagram.m
+++ +mpd/@diagram/diagram.m
@@ -20,16 +20,51 @@ classdef diagram < handle
             
             % open fig
             self.fig = figure();
-            self.fig.Color = [1 1 1]; % white background
+%             self.fig.Color = [1 1 1]; % white background
             
             nChan = length(self.channel_type);
             y_space = 1/nChan;
             
             % set axes, from bottom to top
             for a = nChan : -1 : 1
+                
+                % create axes, the place holder hor each curve type == channel
                 ax(a) = axes(self.fig); %#ok<LAXES>
+                hold(ax(a), 'on')
                 ax(a).OuterPosition = [0.00 (nChan-a)*y_space 1.00 y_space*1.00];
                 ax(a).InnerPosition = [0.05 (nChan-a)*y_space 0.95 y_space*0.90];
+                
+                % now plot
+                switch self.channel_type{a}
+                    case 'RF'
+                        is_obj = cellfun(@(x) isa(x,'mpd.rf_pulse'), self.element_array);  
+                        where_obj = find(is_obj);
+                    case 'G_SS'
+                        is_obj = cellfun(@(x) isa(x,'mpd.gradient'), self.element_array);
+                        where_obj = find(is_obj);
+                        where_obj = where_obj( cellfun(@(x) strcmp(x.type, mpd.grad_type.slice_selection), self.element_array(where_obj)) );
+                    case 'G_PE'
+                        is_obj = cellfun(@(x) isa(x,'mpd.gradient'), self.element_array);
+                        where_obj = find(is_obj);
+                        where_obj = where_obj( cellfun(@(x) strcmp(x.type, mpd.grad_type.phase_encoding), self.element_array(where_obj)) );
+                    case 'G_RO'
+                        is_obj = cellfun(@(x) isa(x,'mpd.gradient'), self.element_array);
+                        where_obj = find(is_obj);
+                        where_obj = where_obj( cellfun(@(x) strcmp(x.type, mpd.grad_type.readout), self.element_array(where_obj)) );
+                    case 'ADC'
+                        is_obj = cellfun(@(x) isa(x,'mpd.adc')     , self.element_array);
+                        where_obj = find(is_obj);
+                    otherwise
+                        error('??')
+                end % switch
+                
+                for i = 1 : numel(where_obj)
+                    obj = self.element_array{where_obj(i)};
+                    plot( ax(a), ...
+                        [obj.onset obj.onset obj.offset obj.offset], ...
+                        [0 1 1 0] )
+                end
+                
                 ax(a).XTick = [];
                 ax(a).YTick = [];
                 ax(a).YLabel.Interpreter = 'none';
@@ -37,8 +72,12 @@ classdef diagram < handle
                 ax(a).XAxis.Color = ax(a).Parent.Color;
                 ax(a).YAxis.Color = ax(a).Parent.Color;
                 ax(a).YLabel.Color = [0 0 0];
-            end
+                
+            end % for
+            
+            linkaxes(ax,'x')
             self.ax = ax; %#ok<*PROP>
+            
         end % function
         
     end % methods
